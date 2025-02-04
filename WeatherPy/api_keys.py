acdc3c2d69450e482443a501dc7ed08c59adcc60
Sharan515 <Sharanjitsing@gmail.com>
2025-02-04 00:09:54 -0500

    Add files via upload

---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,10 @@
+# OpenWeatherMap API Key
+weather_api_key = "d5a29c344add7f4baec0ee245371cb0a"
+
+# Geoapify API Key
+geoapify_key = "53af627fc5eb45378d0fe1f00818d320"
+
+# gmaps API Key
+gmaps_key = "AIzaSyB-Ads_S35hzTMBRGRGxXwkUyKUm-nViwk"
+
+
